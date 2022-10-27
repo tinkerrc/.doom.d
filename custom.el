b8450397b78e7fe39766c0094f8498ea5d607359
Zhenkai Weng <theoaktree1040@gmail.com>
2022-10-26 17:34:50 -0700

    Initial commit

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval when
+      (save-excursion
+        (goto-char
+         (point-min))
+        (re-search-forward "#\\+roam_tags:.*blog" nil t))
+      (org-hugo-auto-export-mode)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
